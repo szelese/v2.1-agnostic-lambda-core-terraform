c9e8a0f5929deaa188f8e14ce0782bb4f958e4c5
szelese <szeles.ervin10@gmail.com>
2026-03-10 17:29:33 +0100

    fix: enable ECR force_delete for seamless infrastructure lifecycle

--- ecr.tf
+++ ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "lambda_core" {
   name                 = "v2-agnostic-lambda-core"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
