9af1ccbeb3f2291900f7b5c24cc4abccef3d3fd1
szelese <szeles.ervin10@gmail.com>
2026-03-10 17:13:00 +0100

    feat: automate GitHub repository secrets via Terraform

--- providers.tf
+++ providers.tf
@@ -5,9 +5,17 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "github" {
+  owner = "szelese" // Update this to your GitHub username or organization
 }
